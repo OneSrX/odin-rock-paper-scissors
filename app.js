92569aeded5b95c241d9f3c3aafb686d0f85c665
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2024-06-24 22:18:47 +0100

    Add players score variables and playRound function
    This will keep track of the players score and handle the logic to play
    a single round of the game

--- app.js
+++ app.js
@@ -16,3 +16,43 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Enter your choice (rock, paper or scissors): ");
 }
+
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound(computerChoice, humanChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (computerChoice === humanChoice) {
+    console.log("It's a tie!");
+  } else if (computerChoice === "rock") {
+    if (humanChoice === "paper") {
+      console.log("You win! Paper beats Rock");
+      humanScore += 1;
+    } else if (humanChoice === "scissors") {
+      console.log("You lose! Rock beats Scissors");
+      computerScore += 1;
+    }
+  } else if (computerChoice === "paper") {
+    if (humanChoice === "rock") {
+      console.log("You lose! Paper beats Rock");
+      computerScore += 1;
+    } else if (humanChoice === "scissors") {
+      console.log("You win! Scissors beat Paper");
+      humanScore += 1;
+    }
+  } else if (computerChoice === "scissors") {
+    if (humanChoice === "rock") {
+      console.log("You win! Rock beats Scissors");
+      humanScore += 1;
+    } else if (humanChoice === "paper") {
+      console.log("You lose! Scissors beat Paper");
+      computerScore += 1;
+    }
+  }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+playRound(computerSelection, humanSelection);
