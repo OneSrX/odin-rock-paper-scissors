de854fc4ec885763e130831530079a34f5ac6510
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2024-06-24 18:57:26 +0100

    Create getComputerChoice function
    This function will randomly returns “rock”, “paper” or “scissors”.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hi Mom!");
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * 3);
+  let choice = "";
+
+  if (randomNum === 0) {
+    choice = "rock";
+  } else if (randomNum === 1) {
+    choice = "paper";
+  } else {
+    choice = "scissors";
+  }
+
+  return choice;
+}
+
+console.log(getComputerChoice());
