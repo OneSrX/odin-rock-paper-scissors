def2902dc2a82811a3e9e04984ccd48a599e246d
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2024-07-25 16:38:25 +0100

    Refactor playRound function
    It's now showing each round outcome in the display area.

--- app.js
+++ app.js
@@ -1,22 +1,68 @@
-const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+
+var humanScore = 0;
+var computerScore = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    const cpuChoice = getCpuChoice();
-    const playerChoice = getPlayerChoice(event.target.textContent);
+    const clickedButton = event.target;
 
-    display.textContent = `CPU: ${cpuChoice}, Player: ${playerChoice}`;
+    const humanSelection = getHumanChoice(clickedButton.textContent);
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
   });
 });
 
-function getPlayerChoice(choice) {
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+
+  return choices[randomIndex];
+}
+
+function getHumanChoice(choice) {
   return choice.toLowerCase();
 }
 
-function getCpuChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomNum = Math.floor(Math.random() * choices.length);
+function playRound(humanChoice, computerChoice) {
+  let roundResult = "";
 
-  return choices[randomNum];
+  if (humanChoice === computerChoice) {
+    roundResult = "It's a tie! No winners here.";
+  }
+
+  switch (humanChoice) {
+    case "rock":
+      if (computerChoice === "paper") {
+        roundResult = "Rock? Paper covers it. Better luck next time!";
+        computerScore++;
+      } else if (computerChoice === "scissors") {
+        roundResult = "Rock beats scissors. Wow, so original!";
+        humanScore++;
+      }
+      break;
+
+    case "paper":
+      if (computerChoice === "rock") {
+        roundResult = "Paper covers rock. Classic win for you!";
+        humanScore++;
+      } else if (computerChoice === "scissors") {
+        roundResult = "Scissors cut paper. Nice choice, though!";
+        computerScore++;
+      }
+      break;
+
+    case "scissors":
+      if (computerChoice === "rock") {
+        roundResult = "Scissors? Rock crushes them!";
+        computerScore++;
+      } else if (computerChoice === "paper") {
+        roundResult = "Paper gets cut. Guess you're just better!";
+      }
+      break;
+  }
+
+  display.textContent = roundResult;
 }
