ad92df1d604ac52b24a4e1143d6ee7d7e4323d9f
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2024-07-25 08:27:05 +0100

    Get and show player & CPU choices on button click
    I also did some refactoring.

--- app.js
+++ app.js
@@ -1,71 +1,22 @@
-function getComputerChoice() {
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const cpuChoice = getCpuChoice();
+    const playerChoice = getPlayerChoice(event.target.textContent);
+
+    display.textContent = `CPU: ${cpuChoice}, Player: ${playerChoice}`;
+  });
+});
+
+function getPlayerChoice(choice) {
+  return choice.toLowerCase();
+}
+
+function getCpuChoice() {
   const choices = ["rock", "paper", "scissors"];
-  const randomNum = Math.floor(Math.random() * 3);
+  const randomNum = Math.floor(Math.random() * choices.length);
 
   return choices[randomNum];
 }
-
-function getHumanChoice() {
-  return prompt("Enter your choice (rock, paper or scissors): ").toLowerCase();
-}
-
-function playGame(rounds = 5) {
-  let computerScore = 0;
-  let humanScore = 0;
-
-  function playRound(computerChoice, humanChoice) {
-    if (computerChoice === humanChoice) {
-      console.log("It's a tie!");
-    } else if (computerChoice === "rock") {
-      if (humanChoice === "paper") {
-        console.log("You win! Paper beats Rock");
-        humanScore += 1;
-      } else if (humanChoice === "scissors") {
-        console.log("You lose! Rock beats Scissors");
-        computerScore += 1;
-      }
-    } else if (computerChoice === "paper") {
-      if (humanChoice === "rock") {
-        console.log("You lose! Paper beats Rock");
-        computerScore += 1;
-      } else if (humanChoice === "scissors") {
-        console.log("You win! Scissors beat Paper");
-        humanScore += 1;
-      }
-    } else if (computerChoice === "scissors") {
-      if (humanChoice === "rock") {
-        console.log("You win! Rock beats Scissors");
-        humanScore += 1;
-      } else if (humanChoice === "paper") {
-        console.log("You lose! Scissors beat Paper");
-        computerScore += 1;
-      }
-    }
-  }
-
-  while (rounds > 0) {
-    const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
-
-    playRound(computerSelection, humanSelection);
-    rounds -= 1;
-  }
-
-  determineWinner(computerScore, humanScore);
-}
-
-function determineWinner(cpuScore, playerScore) {
-  if (cpuScore > playerScore) {
-    console.log(
-      `Game over! You lost. Computer Score: ${cpuScore}, Your Score: ${playerScore}`
-    );
-  } else if (cpuScore < playerScore) {
-    console.log(
-      `Congratulations! You won. Your Score: ${playerScore}, Computer Score: ${cpuScore}`
-    );
-  } else {
-    console.log(`It's a tie! Better luck next time.`);
-  }
-}
-
-playGame();
